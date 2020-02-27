2b94a9795fb33467a103f1d1d8198a75062b2c72
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-27 15:16:06 +0100

    update podio spack recipe

--- packages/podio/package.py
+++ packages/podio/package.py
@@ -1,13 +1,13 @@
 ##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
 # Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
 # LLNL-CODE-647188
 #
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License (as
@@ -22,33 +22,55 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-
 from spack import *
 
 
 class Podio(CMakePackage):
-    """Event data model description library"""
+    """PODIO, or plain-old-data I/O, is a C++ library to support the creation
+    and handling of data models in particle physics."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
-    url      = "https://github.com/HEP-FCC/podio/archive/v0.4.tar.gz"
+    homepage = "https://github.com/AIDASOFT/podio"
+    url      = "https://github.com/AIDASOFT/podio"
 
-    version('0.4.1', '6c3e166bfca6a7d36de05cbde3a55713')
-    version('0.4'  , '9cb8b0dd4510ed1ee05e03982196151d')
-    version('0.3.2', 'ed07d059e0fe79336a8c9c8e8be6d4e1')
-    version('0.3.1', 'a7bc95a99af6fc50ae39539e5aa1087e')
-    version('0.3'  , '04411e2a48126846f576ca292e4656a0')
-    version('develop', git='https://github.com/HEP-FCC/podio.git', branch='master')
+    version('00-09-02', sha256='8234d1b9636029124235ef81199a1220968dcc7fdaeab81cdc96a47af332d240', preferred=True)
+    version('00-09', sha256='3cde67556b6b76fd2d004adfaa3b3b6173a110c0c209792bfdb5f9353e21076f')
+    version('00-08', sha256='9d035a7f5ebfae5279a17405003206853271af692f762e2bac8e73825f2af327')
+    version('develop', git='https://github.com/AIDASOFT/podio.git', branch='master')
 
-    depends_on('cmake', type='build')
-    depends_on('py-pyyaml', type='run')
-    depends_on('root')
+    variant('build_type', default='Release',
+            description='The build type to build',
+            values=('Debug', 'Release'))
 
-    def configure_args(self):
-        spec = self.spec
-        return [
-            '-DCMAKE_BUILD_TYPE:STRING=%s' ('Debug' if '+debug' in spec else 'Release')
-        ]
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    depends_on('cmake')
+    depends_on('root@6.08.06:')
+    depends_on('python@2.7:')
+    depends_on('py-pyyaml')
+    depends_on('tbb')
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
+
+    # in LCG_96 ROOT is installed with an external xz rather than the builtin,
+    # so the genreflex binary needs to find it.
+    # As root is installed as an external package we cannot modify its
+    # setup_dependent_environment function to add the xz lib folder to the
+    # LD_LIBRARY_PATH hence we need to do it here.
+    depends_on('xz', when='^root@6.16:')
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('PODIO', self.prefix)
+
+    def setup_environment(self, spack_env, run_env):
+      spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      if 'xz' in self.spec:
+        spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
